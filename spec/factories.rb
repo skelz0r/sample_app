6d62f808569e475fe6e1b52c743b5f5a2646f55c
Mebius <viddafroygeo@hotmail.com>
2014-04-06 23:15:55 +0200

    Change a little thing concerning a factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,19 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 FactoryGirl.define do 
+
   sequence :email do |n|
     "person#{n}@example.com"
   end
+
   factory :user do
     name "Michael Hartl"
     email 
     password "foobar"
     password_confirmation "foobar"
   end
+
+  factory :micropost do
+    content "MyString"
+  end
+
 end
